41281f2a886da1ae8d79e0c7d14692419334ab56
Alvaro Peña <me@alvropena.com>
2025-07-24 22:02:27 -0700

    Rename function PrivatePage to HomePage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation'
 
 import { createClient } from '@/utils/supabase/server'
 
-export default async function PrivatePage() {
+export default async function HomePage() {
     const supabase = await createClient()
 
     const { data, error } = await supabase.auth.getUser()
